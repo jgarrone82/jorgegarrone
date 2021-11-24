899317f67bdb2ec83bac6e2d2e06392897a6265d
jgarrone82 <jgarrone@logytechmobile.com>
2021-11-24 16:54:01 -0300

    Add: modified styles, and html

--- index.js
+++ index.js
@@ -19,3 +19,31 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log('listening.....');
 })
+
+app.post('/mail', (req, res) => {
+  const { firstname, lastname, email, msg } = req.body;
+
+  const transporter = nodemailer.createTransport({
+      service: 'gmail',
+      auth: {
+          user: process.env.EMAIL,
+          pass: process.env.PASSWORD
+      }
+  })
+
+  const mailOptions = {
+      from: `${email}`,
+      to: process.env.EMAIL,
+      subject: 'Portfolio',
+      text: `First name: ${firstname}, \nLast name: ${lastname}, \nEmail: ${email}, \nMessage: ${msg}`
+  }
+  console.log(mailOptions);
+  transporter.sendMail(mailOptions, (err, result) => {
+      if (err){
+          console.log(err);
+          res.json('Opps! it seems like some error occured. Please try again.')
+      } else{
+          res.json('Thanks for e-mailing me. I will reply to you within 2 working days');
+      }
+  })
+})
---
+++ public/project.js
@@ -0,0 +1,13 @@
+let projects = [
+  {
+      name: "project one",
+      tags: "#javascript, #fullstack, #ui/ux, #backend",
+      image: "project (1).jpg",
+  },
+  {
+      name: "project two",
+      tags: "#javascript, #fullstack",
+      image: "project (1).jpg",
+  },
+  // +8 more
+]
