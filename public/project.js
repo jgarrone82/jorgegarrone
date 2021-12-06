01b55cafcf709b296b280387679879c06ed79516
jgarrone82 <jgarrone@logytechmobile.com>
2021-12-06 12:47:30 -0300

    added imgs, modified cv and index

--- public/project.js
+++ public/project.js
@@ -12,16 +12,17 @@ let projects = [
     link: "https://github.com/jgarrone82/Tienda-Online",
   },
   {
-    name: "NodeJs-Blog",
-    tags: "#javascript, #fullstack",
+    name: "Vue-project ",
+    tags: "#javascript, #fullstack, #ui/ux, #backend",
     image: "project (3).jpg",
-    link: "https://github.com/jgarrone82/NodeJs-Blog",
+    link: "https://github.com/jgarrone82/Vue-project",
   },
   {
-    name: "Vue-project ",
+    name: "Portfolio",
     tags: "#javascript, #fullstack, #ui/ux, #backend",
     image: "project (4).jpg",
     link: "https://github.com/jgarrone82/Vue-project",
   },
+
 
 ]
