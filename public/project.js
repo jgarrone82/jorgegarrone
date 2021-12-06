1b5f1dad857be9039bf243a34495a15ac05cce6e
jgarrone82 <jgarrone@logytechmobile.com>
2021-12-06 12:59:33 -0300

    fix: changed name of pics

--- public/project.js
+++ public/project.js
@@ -2,25 +2,25 @@ let projects = [
   {
     name: "platzi-backend-node",
     tags: "#javascript, #backend",
-    image: "project (1).jpg",
+    image: "project(1).jpg",
     link: "https://github.com/jgarrone82/platzi-backend-node",
   },
   {
     name: "Online-Store",
     tags: "#javascript, #fullstack, #ui/ux, #backend",
-    image: "project (2).jpg",
+    image: "project(2).jpg",
     link: "https://github.com/jgarrone82/Tienda-Online",
   },
   {
     name: "Vue-project ",
     tags: "#javascript, #fullstack, #ui/ux, #backend",
-    image: "project (3).jpg",
+    image: "project(3).jpg",
     link: "https://github.com/jgarrone82/Vue-project",
   },
   {
     name: "Portfolio",
     tags: "#javascript, #fullstack, #ui/ux, #backend",
-    image: "project (4).jpg",
+    image: "project(4).jpg",
     link: "https://github.com/jgarrone82/Vue-project",
   },
 
